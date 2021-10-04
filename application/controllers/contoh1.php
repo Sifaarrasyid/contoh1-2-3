37753d1f96f8350241361160e7976768a816e72d
sifaarrasyid <sifaarrasyid26@gmail.com>
2021-10-04 19:57:50 +0700

    tugas contoh 1,2,3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya sifa arrasyid,
+        Saya tinggal di majalengka. Olahraga
+        yang paling saya sukai yaitu lari karena sangat menyenangkan dan refresh badan.";
+    }
+}
